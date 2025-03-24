9707cacd02948701ffe8254d270d71337f8714e1
Nolan Holden <holden.nolan@gmail.com>
2025-03-23 23:38:51 -0700

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://chatgpt4kids.com",
-      lastModified: "2025-03-16T00:29:13.795Z",
+      lastModified: "2025-03-24T05:38:02.920Z",
       changeFrequency: "daily",
       priority: 1,
     },
@@ -21,6 +21,16 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://chatgpt4kids.com/register",
       lastModified: "2025-03-16T00:29:13.795Z",
+      priority: 0.1,
+    },
+    {
+      url: "https://chatgpt4kids.com/privacy-policy",
+      lastModified: "2025-03-24T06:38:02.920Z",
+      priority: 0.1,
+    },
+    {
+      url: "https://chatgpt4kids.com/terms-of-use",
+      lastModified: "2025-03-24T06:38:02.920Z",
       priority: 0.1,
     },
   ];
